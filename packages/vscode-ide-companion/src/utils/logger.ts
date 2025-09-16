3ed4f2a17ee1852c330a3ad7542dd8441989c676
oli <oli@hlmpn.dev>
2025-09-17 01:13:30 +0200

    fix: regenerated stale lockfile

--- packages/vscode-ide-companion/src/utils/logger.ts
+++ packages/vscode-ide-companion/src/utils/logger.ts
@@ -4,15 +4,15 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function createLogger(
-  context: vscode.ExtensionContext,
-  logger: vscode.OutputChannel,
+	context: vscode.ExtensionContext,
+	logger: vscode.OutputChannel,
 ) {
-  return (message: string) => {
-    if (context.extensionMode === vscode.ExtensionMode.Development) {
-      logger.appendLine(message);
-    }
-  };
+	return (message: string) => {
+		if (context.extensionMode === vscode.ExtensionMode.Development) {
+			logger.appendLine(message);
+		}
+	};
 }
