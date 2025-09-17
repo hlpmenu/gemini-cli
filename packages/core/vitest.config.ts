3d6ea1bdc1f1c435e83f4357eafbdc852504e187
oli <oli@hlmpn.dev>
2025-09-17 02:55:17 +0200

    switched coverage provider to bun supported 'istanbul' for vitest

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -4,29 +4,29 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    reporters: ['default', 'junit'],
-    silent: true,
-    setupFiles: ['./test-setup.ts'],
-    outputFile: {
-      junit: 'junit.xml',
-    },
-    coverage: {
-      enabled: true,
-      provider: 'v8',
-      reportsDirectory: './coverage',
-      include: ['src/**/*'],
-      reporter: [
-        ['text', { file: 'full-text-summary.txt' }],
-        'html',
-        'json',
-        'lcov',
-        'cobertura',
-        ['json-summary', { outputFile: 'coverage-summary.json' }],
-      ],
-    },
-  },
+	test: {
+		reporters: ["default", "junit"],
+		silent: true,
+		setupFiles: ["./test-setup.ts"],
+		outputFile: {
+			junit: "junit.xml",
+		},
+		coverage: {
+			enabled: true,
+			provider: "istanbul",
+			reportsDirectory: "./coverage",
+			include: ["src/**/*"],
+			reporter: [
+				["text", { file: "full-text-summary.txt" }],
+				"html",
+				"json",
+				"lcov",
+				"cobertura",
+				["json-summary", { outputFile: "coverage-summary.json" }],
+			],
+		},
+	},
 });
